eb12ed5599bc00a15403d789e5624281e34393cc
layjen6644 <layjen6644@students.ecpi.edu>
2026-04-19 14:23:07 -0400

    Week 2

--- App.java
+++ App.java
@@ -1,29 +1,51 @@
 /* 
 Name: Layla Jenkins
-Date: 04/12/2026
-Assignment: Week 1 - Lab Project
+Date: 04/19/2026
+Assignment: 2.5 Performance Assessment_Loops & Calculations
 
-This is the first portion of my project for the 
-lab assignment. I'll include a welcome message and
-instructions for the user. It will also include some 
-beginner functions for calculating values */
-
+Main application (program) class.
+This program will demonstrate two types of loops, counter controlled
+and condition based loops. */
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Layla Jenkins - Week 1 Lab Project");
+        System.out.println("Layla Jenkins - Week 2 PA Loops & Calculations");
         System.out.println();
-        System.out.println("Dear Math, I'm not a therapist. Solve your own problems.");
+
+        //Demonstrate a counter-controlled loop
+        System.out.println("Counter-Controlled loop:");
+        System.out.println("Calculating the sum of integers 1-10:");
+        //Initializing int (data type) sum (variable) to start at 0
+        int sum = 0;
+        //For loop to add numbers 1-10 together
+        for (int cnt =1; cnt <= 10; cnt++) {
+            sum += cnt;
+            System.out.printf("Running total after " + cnt + " = " + sum + "\n");
+        }
+        System.out.printf("Final Total: %d%n", sum);
         System.out.println();
-        System.out.println("This is a math calculator to solve your problems so you don't have to.");
-        System.out.println();
-        //Creates calculator object
-        Calculator calculator = new Calculator();
-        //Calls methods for addition and subtraction
-        calculator.add();
-        calculator.subtract();
-        //End of program
-        System.out.println("End of program. Remember: It's ok not to have all the answers!");
-    }
+        //Demonstrate a condition-based loop
+        System.out.println("Condition-based loop:");
+        //Creat a scanner to get user input
+        Scanner input = new Scanner(System.in);
+        //Initializing runningTotal and countto start at 0
+        int runningTotal = 0;
+        int count = 0;
+        int inVal;
+        //Do..while loop to add user input together and count total entries
+        do {
+            System.out.println("Enter an integer value (-1 to quit): ");
+            inVal = input.nextInt();
+
+            if (inVal != -1) {
+                runningTotal += inVal;
+                count++;
+                System.out.printf("Running total: %d%n", runningTotal);
+            }
+        } while (inVal != -1);
+        
+        System.out.printf("Final Total: %d%n", runningTotal);
+        System.out.printf("The number of entries was: %d", count);
+    }   
 }
