40042e0ec28dadb0cb4a3d75dd1f127d382cd8ab
layjen6644 <layjen6644@students.ecpi.edu>
2026-04-23 19:56:31 -0400

    Week 3, assignment 3.5 Performance Assessment

--- App.java
+++ App.java
@@ -1,51 +1,99 @@
-/* 
+/*
 Name: Layla Jenkins
-Date: 04/19/2026
-Assignment: 2.5 Performance Assessment_Loops & Calculations
+Date: 04/23/2026
+Assignment: 3.5 Performance Assessment_Calculations & Unique Numbers
 
 Main application (program) class.
-This program will demonstrate two types of loops, counter controlled
-and condition based loops. */
+In this application, we will demonstrate the use of array and ArrayList objects 
+and using methods to make calculations of elements in the array and ArrayList. 
+*/
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Layla Jenkins - Week 2 PA Loops & Calculations");
+        System.out.println("Layla Jenkins - Week 3 PA - Calculations & Unique Numbers");
         System.out.println();
 
-        //Demonstrate a counter-controlled loop
-        System.out.println("Counter-Controlled loop:");
-        System.out.println("Calculating the sum of integers 1-10:");
-        //Initializing int (data type) sum (variable) to start at 0
-        int sum = 0;
-        //For loop to add numbers 1-10 together
-        for (int cnt =1; cnt <= 10; cnt++) {
-            sum += cnt;
-            System.out.printf("Running total after " + cnt + " = " + sum + "\n");
-        }
-        System.out.printf("Final Total: %d%n", sum);
-        System.out.println();
-        //Demonstrate a condition-based loop
-        System.out.println("Condition-based loop:");
-        //Creat a scanner to get user input
+        //Declare a scanner to get user input
         Scanner input = new Scanner(System.in);
-        //Initializing runningTotal and countto start at 0
-        int runningTotal = 0;
-        int count = 0;
-        int inVal;
-        //Do..while loop to add user input together and count total entries
-        do {
-            System.out.println("Enter an integer value (-1 to quit): ");
-            inVal = input.nextInt();
 
-            if (inVal != -1) {
-                runningTotal += inVal;
-                count++;
-                System.out.printf("Running total: %d%n", runningTotal);
-            }
-        } while (inVal != -1);
+        //Ask the user for 10 integers
+        System.out.println("Enter 10 integers to find the sum: ");
+        //create an array to hold the 10 integers
+        int[] integersArr = new int[10];
+        //loop 10 times to get the integers from the user and store them in the array
+        for (int i = 0; i < integersArr.length; i++) {
+            System.out.print("Integer " + (i + 1) + ": ");
+            integersArr[i] = input.nextInt();
+        }   
+
+        //Call method to calculate the sum of the integers in the array
+        int sumArr = sumArray(integersArr);
+        //Displays the size of the array
+        System.out.printf("The number of integers you entered is: %d%n", integersArr.length);
+        //display the sum to the user
+        System.out.printf("The sum of the integers you entered is: %d%n", sumArr);
+        //Displays the average of the array
+        double averageArr = (double) sumArr/integersArr.length;
+        System.out.printf("The average of the integers you entered is: %.2f%n", averageArr);
         
-        System.out.printf("Final Total: %d%n", runningTotal);
-        System.out.printf("The number of entries was: %d", count);
-    }   
+        //Getting unique (numbers) integers for an ArrayList, using overload of getUniqueNumbers that doesn't require a count of integers to be entered
+        System.out.println();
+        System.out.println("Enter integers to find the unique numbers (enter -1 to stop): ");
+        //create an ArrayList to hold the integers
+        ArrayList<Integer> uniqueNumbers = getUniqueList();
+        int sum = sumArrayList(uniqueNumbers);
+        //Displays the size of ArrayList
+        System.out.printf("The number of unique integers you entered is: %d%n", uniqueNumbers.size());
+        //display the sum to the user
+        System.out.printf("The sum of the integers you entered is: %d%n", sum);
+        //Displays the average of the ArrayList
+        double average = (double) sum/uniqueNumbers.size();
+        System.out.printf("The average of the integers you entered is: %.2f%n", average);
+    }
+
+      //Method to calculate the sum of integers in an array
+    public static int sumArray(int[] arr) {
+        int sum = 0;  //Initializing sum to 0
+        //loop through the array and add each integer to the sum
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    //Overload function and method to get unique numbers from the user and return an ArrayList
+    public static ArrayList<Integer> getUniqueList() {
+        //declare a scanner to get user input
+        Scanner input = new Scanner(System.in);
+
+        //create an ArrayList to hold the unique numbers
+        ArrayList<Integer> uniqueList = new ArrayList<>();
+        //declares integer variable 
+        int number;
+
+        //loop to get integers from the user until they enter -1 to stop
+        do {
+            System.out.print("Enter an integer (or -1 to stop): ");
+            number = input.nextInt();
+            //if the number is not already in the ArrayList, add it (contains)
+            if (number != -1) {
+                if (!uniqueList.contains(number)) {
+                    uniqueList.add(number);
+                }
+            }
+        } while (number != -1);  //Will continue until user enters -1 to stop
+        return uniqueList;
+    }
+
+    //Overload function to calculate sum of ArrayList
+    public static int sumArrayList(ArrayList<Integer> list) {
+        int sum = 0;  //Initializing sum to 0
+        //loop through the ArrayList and add each integer to the sum
+        for (int num : list) {
+            sum += num;
+        }
+        return sum;
+    }
 }
